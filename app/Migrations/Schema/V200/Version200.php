79fb0e1c3c03c55679ce05863d748f1e66b23daa
Miguel C <miguel.catalan@enzymeadvisinggroup.com>
2017-05-02 11:49:34 +0200

    Feature, add field in gradebook certificate table, which saves datetime about when the user see for first time the gradebook certificate.

--- app/Migrations/Schema/V200/Version200.php
+++ app/Migrations/Schema/V200/Version200.php
@@ -199,6 +199,9 @@ class Version200 extends AbstractMigrationChamilo
 
         $this->addSql("ALTER TABLE c_quiz_question_category CHANGE description description LONGTEXT DEFAULT NULL;");
 
+        //Add field in gradebook certificate table, which saves datetime about when the user see for first time the gradebook certificate.
+        $this->addSql("ALTER TABLE `gradebook_certificate` ADD `viewed_at` DATETIME DEFAULT NULL");
+
     }
 
     /**
